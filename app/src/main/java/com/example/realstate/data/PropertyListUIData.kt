f329b4214009f546abfb8edc80397a3a09a0974a
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2023-03-15 15:22:43 +0100

    property list ui data created

---
+++ app/src/main/java/com/example/realstate/data/PropertyListUIData.kt
@@ -0,0 +1,11 @@
+package com.example.realstate.data
+
+data class PropertyListUIData(
+    val id: Int,
+    val image: String,
+    val price: Double,
+    val city: String,
+    val rooms: Int,
+    val bedrooms: Int,
+    val area: Double,
+)
